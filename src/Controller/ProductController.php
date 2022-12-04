dd66f7c3cdcc8f2a480de7fd6fd352cc902f0b6f
fazriachyar <fazriachyar@yandex.com>
2022-12-04 08:34:45 +0700

    add findById

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -19,21 +19,42 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'view_product', methods: ['GET', 'HEAD'])]
+    #[Route('/product/view', name: 'view_product', methods: ['GET', 'HEAD'])]
     public function viewAllProduct(ManagerRegistry $doctrine): Response
     {
         $em = $doctrine->getManager();
         $normalizer = new ObjectNormalizer();
         $encoder = new JsonEncoder();
-        
-        $serializer = new Serializer([$normalizer], [$encoder]);
 
         $viewAllProduct = $em->getRepository(Product::class)
             ->findAll();
+        if(!$viewAllProduct){
+            $response = new Response("Not Found", Response::HTTP_OK);
+        }
 
+        $serializer = new Serializer([$normalizer], [$encoder]);
         $data = $serializer->serialize($viewAllProduct, 'json');
         $response = new Response($data, Response::HTTP_OK);
 
         return $response;
+    }
+
+    #[Route('/product/{id}', name: 'add_product', methods: ['GET'])]
+    public function addProduct(ManagerRegistry $doctrine,int $id): Response
+    {
+        $em = $doctrine->getManager();
+        $normalizer = new ObjectNormalizer();
+        $encoder = new JsonEncoder();
+
+        $findProductById = $em->getRepository(Product::class)
+            ->findOneBy([
+                'id' => $id
+            ]);
+        
+        $serializer = new Serializer([$normalizer], [$encoder]);
+        $data = $serializer->serialize($findProductById, 'json');
+        $response = new Response($data, Response::HTTP_OK);
+
+        return $response
     }
 }
